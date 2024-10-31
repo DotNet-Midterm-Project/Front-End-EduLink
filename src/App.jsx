a2dcb24962eb361783bf129a8d3a76d3d7b5f413
Hassan Karraz <24042732@student.ltuc.com>
2024-10-31 11:41:43 +0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import './App.css'
+
 
 function App() {
-  
   return (
     <>
       <h1>Hello world</h1>
       <h1>Edu Link Project</h1>
+      <h1 className="mx-auto font-bold underline bg-red-500">
+        Hello world! From Tailwind
+      </h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
