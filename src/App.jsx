ae9f8474c05fa46d3f345060a5076f6e7226c67b
put your github username here <muathmhawich1@gmail.com>
2024-11-22 23:36:05 +0300

    create admin dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,62 @@
 import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import AdminLayout from "./AdminLayout";
+import AdminViewAllCourses from "./Pages/Admin/Admin View Courses";
+import AdminViewAllDepartments from "./Pages/Admin/AdminViewAllDepartments";
+import AdminViewAllVolunteers from "./Pages/Admin/AdminViewAllVolunteer";
 import ArticlesList from "./Pages/Article/ArticlesList";
 import ArticlesListByID from "./Pages/Article/ArticlesListByID";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home/Home";
-import Layout from './Components/Layout';
-import AboutUs from "./Pages/AboutUs";
-import StudentPage from "./Pages/Student/StudentPage";
-import Profile from "./Pages/Profile";
+import Layout from "./Components/Layout";
 import Login from "./Pages/Auth/Login";
 import Register from "./Pages/Auth/Register";
 import VerifyEmailPage from "./Components/Common/verifyEmail";
 import ResetPassword from "./Pages/Auth/ResetPassword";
+import AboutUs from "./Pages/AboutUs";
+import StudentPage from "./Pages/Student/StudentPage";
+import Profile from "./Pages/Profile";
 import BookingPage from "./Pages/Student/BookingPage";
 import EventPage from "./Pages/Student/EventPage";
-import EventContentPage  from "./Pages/Student/EventContentPage";
+import EventContentPage from "./Pages/Student/EventContentPage";
 import ShowVolunteerByCourse from "./Components/Student/ShowVolunteerByCourse";
 import YourEvent from "./Pages/Student/YourEvent";
-
-
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { fas } from "@fortawesome/free-solid-svg-icons";
-import { fab } from "@fortawesome/free-brands-svg-icons";
 import Contact from "./Pages/contactUS/Contact";
-
-// Add all icons to the library
-library.add(fas, fab);
-
+import AdminArticles from "./Pages/Admin/AdminViewArticles";
 
 const App = () => {
-  
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/reset-password" element={<Layout><ResetPassword /></Layout>} />
-          <Route path="/login" element={<Layout><Login /></Layout>} />
-          <Route path="/register" element={<Layout><Register /></Layout>} />
-          <Route path="/verify" element={<Layout><VerifyEmailPage /></Layout>} />
-          <Route path="/articles" element={<Layout><ArticlesList /></Layout>} />
-          <Route path="/articles/:id" element={<Layout><ArticlesListByID /></Layout>} />
-          <Route path="/" element={<Home />}/>
-          <Route path="/about-us" element={<Layout><AboutUs /></Layout>} />
-          <Route path="/student-page" element={<Layout><StudentPage/></Layout>}/>
-          <Route path="/profile" element={<Profile/>}/>
-          <Route path="/booking-page" element={<Layout><BookingPage/></Layout>}/>
-          <Route path="/event-page" element={<Layout><EventPage/></Layout>}/>
-          <Route path="/event-content/:eventId" element={<EventContentPage />} />
-          <Route path="/volunteers/:courseId" element={<ShowVolunteerByCourse />} />
-          <Route path="/your-event-page" element={<Layout><YourEvent/></Layout>} />
-          <Route path="/contact-us" element={<Layout><Contact/></Layout>}/>
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Routes>
+
+        <Route path="/admin" element={<AdminLayout />}>
+        
+          <Route index element={<Navigate to="volunteers" replace />} /> 
+          <Route path="courses" element={<AdminViewAllCourses />} />
+          <Route path="departments" element={<AdminViewAllDepartments />} />
+          <Route path="Articles" element={<AdminArticles />} />
+          <Route path="volunteers" element={<AdminViewAllVolunteers />} />
+        </Route>
+
+
+        <Route path="/" element={<Layout><Home /></Layout>} />
+        <Route path="/reset-password" element={<Layout><ResetPassword /></Layout>} />
+        <Route path="/login" element={<Layout><Login /></Layout>} />
+        <Route path="/register" element={<Layout><Register /></Layout>} />
+        <Route path="/verify" element={<Layout><VerifyEmailPage /></Layout>} />
+        <Route path="/articles" element={<Layout><ArticlesList /></Layout>} />
+        <Route path="/articles/:id" element={<Layout><ArticlesListByID /></Layout>} />
+        <Route path="/about-us" element={<Layout><AboutUs /></Layout>} />
+        <Route path="/student-page" element={<Layout><StudentPage /></Layout>} />
+        <Route path="/profile" element={<Layout><Profile /></Layout>} />
+        <Route path="/booking-page" element={<Layout><BookingPage /></Layout>} />
+        <Route path="/event-page" element={<Layout><EventPage /></Layout>} />
+        <Route path="/event-content/:eventId" element={<Layout><EventContentPage /></Layout>} />
+        <Route path="/volunteers/:courseId" element={<Layout><ShowVolunteerByCourse /></Layout>} />
+        <Route path="/your-event-page" element={<Layout><YourEvent /></Layout>} />
+        <Route path="/contact-us" element={<Layout><Contact /></Layout>} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
---
+++ src/Redux/Slices/adminDepartmentSlice.js
@@ -0,0 +1,46 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const fetchAllDepartment = createAsyncThunk(
+    'departments/fetchAllDepartment',
+    async (_, { rejectWithValue }) => {
+        try {
+            const response = await axios.get(`${import.meta.env.VITE_URL_BACKEND}/api/Admin/get-all-departments?`,
+                { headers: { Authorization: `Bearer ${localStorage.getItem('token')}` } }
+            );
+            console.log(response.data);
+            
+            return response.data;
+        } catch (error) {
+            return rejectWithValue(error.response ? error.response.data : { message: "An error occurred" })
+        }
+    }
+);
+
+const departmentSlice = createSlice({
+    name: "departments",
+    initialState: {
+        departments: [],
+        selectedCourse: null,
+        loading: false,
+        error: ''
+    },
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+        .addCase(fetchAllDepartment.pending, (state) => {
+            state.loading = true;
+            state.error = '';
+        })
+        .addCase(fetchAllDepartment.fulfilled, (state, action) => {
+            state.loading = false;
+            state.departments = action.payload;
+        })
+        .addCase(fetchAllDepartment.rejected, (state, action) => {
+            state.loading = false;
+            state.error = action.payload.message;
+        })
+     
+    }
+})
+export default departmentSlice.reducer;
